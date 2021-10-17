499a0d0960846aedbb33903d8c38e615b49a2a66
Jonas Brito <jonasexplore@gmail.com>
2021-10-17 18:34:44 -0300

    feat: add local database

---
+++ src/infra/database/index.js
@@ -0,0 +1,21 @@
+const database = {
+  costumers: [
+    {
+      id: 1,
+      name: "Joao",
+      birthdate: "2010-05-12",
+    },
+    {
+      id: 1,
+      name: "Paulo",
+      birthdate: "2004-12-01",
+    },
+    {
+      id: 1,
+      name: "Julia",
+      birthdate: "1998-03-24",
+    },
+  ],
+};
+
+module.exports = database;
