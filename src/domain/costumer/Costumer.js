51cf2de5d92e918837965da21c5e0dec76348997
Jonas Brito <jonasexplore@gmail.com>
2021-10-18 08:16:31 -0300

    chore: add created_at and updated_at

--- src/domain/costumer/Costumer.js
+++ src/domain/costumer/Costumer.js
@@ -4,14 +4,21 @@ class Costumer {
   id;
   name;
   birthdate;
+  created_at;
+  updated_at;
 
   constructor(name, birthdate) {
     if (!this.id) {
       this.id = uuid();
     }
 
+    if (!this.created_at) {
+      this.created_at = new Date().toISOString();
+    }
+
     this.name = name;
     this.birthdate = birthdate;
+    this.updated_at = new Date().toISOString();
   }
 }
 
